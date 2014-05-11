2124f27506a5dc29f5a98b17f14257ffa3323dd3
Luke Paireepinart <lukepdev@live.com>
2014-05-11 12:40:36 -0500

    Converted all spaces to tabs

--- main.py
+++ main.py
@@ -9,61 +9,61 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
 #world object
 class World(object):
-	def __init__(self, screen, bgcolor):
-		self.screen = screen
-		self.bgcolor = bgcolor
-		
-	def render(self):
-		self.screen.fill(self.bgcolor)
-		
+    def __init__(self, screen, bgcolor):
+        self.screen = screen
+        self.bgcolor = bgcolor
+
+    def render(self):
+        self.screen.fill(self.bgcolor)
+
 #menu object
 class Menu(object):
-	def __init__(self, screen, bgcolor):
-		self.screen = screen
-		self.bgcolor = bgcolor
-		
-	def render(self):
-		self.screen.fill(self.bgcolor)
+    def __init__(self, screen, bgcolor):
+        self.screen = screen
+        self.bgcolor = bgcolor
+
+    def render(self):
+        self.screen.fill(self.bgcolor)
 
 world = World(screen, (255,255,200))
 
 
 #main game loop
 running = True
-font = pygame.font.Font("PressStart2P.ttf", FONTSIZE) 
+font = pygame.font.Font("PressStart2P.ttf", FONTSIZE)
 clock = pygame.time.Clock()
 
 while running:
-	keys = []
-	#event processing
-	for event in pygame.event.get():
-		if event.type == KEYDOWN:
-			if event.key == K_ESCAPE:
-				pygame.quit()
-				running = False
-			else:
-				keys.append(event.key)
-		if event.type == QUIT:
-			pygame.quit()
-			running = False
-	if not running: break
+    keys = []
+    #event processing
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                pygame.quit()
+                running = False
+            else:
+                keys.append(event.key)
+        if event.type == QUIT:
+            pygame.quit()
+            running = False
+    if not running: break
 
-	#determine the # of game ticks since last frame.
-	print clock.get_time()
-	clock.tick()
-	
-	world.render()
-	
-	#if FPS is on, render it
-	if SHOWFPS:
-		fps = clock.get_fps()
-		if math.isinf(fps):
-			fps = 10000.0
+    #determine the # of game ticks since last frame.
+    print clock.get_time()
+    clock.tick()
+
+    world.render()
+
+    #if FPS is on, render it
+    if SHOWFPS:
+        fps = clock.get_fps()
+        if math.isinf(fps):
+            fps = 10000.0
 
                 screen_rect = screen.get_rect()
                 fps_surf = font.render("%0.1f" % fps, False, (255,255,255))
                 fps_rect = fps_surf.get_rect()
                 fps_rect.topright = screen_rect.move(-5, 5).topright
                 screen.blit(fps_surf, fps_rect)
-                
-	pygame.display.update()
+
+    pygame.display.update()
