562456c476e949e9b0d96544eaba0b0646bfc493
Luke Paireepinart <lukepdev@live.com>
2014-05-11 10:13:28 -0500

    added a json level (blank) and some config settings & beginning of class

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
 #imports
-import pygame
+import pygame, math, json
 from pygame.locals import *
 from config import *
+
 #setup code
 pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
+#world object
+class World(object):
+	def __init__(self, screen, bgcolor):
+		self.screen = screen
+		self.bgcolor = bgcolor
+		
+	def render(self):
+		self.screen.fill(self.bgcolor)
+		
+#menu object
+class Menu(object):
+	def __init__(self, screen, bgcolor):
+		self.screen = screen
+		self.bgcolor = bgcolor
+		
+	def render(self):
+		self.screen.fill(self.bgcolor)
 
+world = World(screen, (255,255,200))
 
 
 #main game loop
 running = True
-font = pygame.font.Font("PressStart2P.ttf", 10) 
+font = pygame.font.Font("PressStart2P.ttf", FONTSIZE) 
 clock = pygame.time.Clock()
 
 while running:
@@ -30,16 +49,17 @@ while running:
 	if not running: break
 
 	#determine the # of game ticks since last frame.
-	
+	print clock.get_time()
 	clock.tick()
 	
-	
-	screen.fill((0,255,255))
+	world.render()
 	
 	#if FPS is on, render it
 	if SHOWFPS:
 		fps = clock.get_fps()
 		try:
-			screen.blit(font.render("%0.1f" % fps, False, (255,255,255)), (200,200))
+			if math.isinf(fps):
+				fps = 10000.0
+			screen.blit(font.render("%0.1f" % fps, False, (255,255,255),  (0,0,0)), (200,200))
 		except: pass
 	pygame.display.update()
